b38bf1e2d4a7c9501ef5d9b302164f621cb933d4
Arya Karimi <devaryakrm@gmal.com>
2022-09-16 23:43:13 +0300

    codes added

---
+++ exercise.js
@@ -0,0 +1,11 @@
+let number = 7;
+
+if (number > 0) {
+  console.log("The number is positive");
+} else console.log("The number is negative");
+
+// ----------This is what I done :) ---------------
+
+let answer = number > 0 ? "The number is positive" : "The number is negative";
+
+console.log(answer);
